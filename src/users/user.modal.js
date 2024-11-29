71f947c512697248d61ab51368059c421a3de5bc
Le Van Cong <conglevan21072001@innofly-software.com>
2024-11-29 15:58:56 +0700

    add register , check login user password

--- src/users/user.modal.js
+++ src/users/user.modal.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({
   username: { type: String, require: true, unique: true },
@@ -13,6 +14,19 @@ const userSchema = new Schema({
     default: Date.now,
   },
 });
+
+//hashing password
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  user.password = hashedPassword;
+  next();
+});
+
+userSchema.methods.comparePassword = function (cadidatePassword) {
+  return bcrypt.compare(cadidatePassword, this.password);
+};
 
 const User = new model("User", userSchema);
 module.exports = User;
--- src/users/user.route.js
+++ src/users/user.route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const User = require("./user.modal");
 const router = express.Router();
 
 //Register endpoint
@@ -8,7 +9,23 @@ router.post("/register", async (req, res) => {
     const user = new User({ email, username, password });
     await user.save();
     res.status(201).send({ message: "User registed successfully!" });
-  } catch (error) {}
+  } catch (error) {
+    console.log("errror", error);
+    res.status(500).send({ message: "Error registering user" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return res.status(401).send({ message: "Password not match" });
+  }
+  res.status(200).send({ message: "Loggin successful", user });
 });
 
 module.exports = router;
