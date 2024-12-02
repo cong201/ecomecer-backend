61952ef57a97e433e75dae55dc909e39dbbcc4e1
Le Van Cong <conglevan21072001@innofly-software.com>
2024-12-02 17:20:14 +0700

    set jwt for user

--- src/users/user.route.js
+++ src/users/user.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("./user.modal");
+const generateToken = require("../middleware/generateToken");
 const router = express.Router();
 
 //Register endpoint
@@ -17,15 +18,39 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) {
-    return res.status(404).send({ message: "User not found" });
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).send({ message: "Password not match" });
+    }
+    const token = await generateToken(user._id);
+    res.cookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+    //RESPONE IN POSTMAN
+    res.status(200).send({
+      message: "Loggin successful",
+      token,
+      user: {
+        _id: user._id,
+        email: user.email,
+        username: user.username,
+        role: user.role,
+        profileImage: user.profileImage,
+        bio: user.bio,
+        profession: user.profession,
+      },
+    });
+  } catch (error) {
+    console.log("Error login in user", error);
+    res.status(500).send({ message: "Error login in user" });
   }
-  const isMatch = await user.comparePassword(password);
-  if (!isMatch) {
-    return res.status(401).send({ message: "Password not match" });
-  }
-  res.status(200).send({ message: "Loggin successful", user });
 });
 
 module.exports = router;
