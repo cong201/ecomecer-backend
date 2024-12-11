37656b7e5a1955f361befdcbc358573be9e802bd
Le Van Cong <conglevan21072001@innofly-software.com>
2024-12-11 19:31:58 +0700

    get single product add

--- src/products/products.route.js
+++ src/products/products.route.js
@@ -68,4 +68,26 @@ router.get("/", async (req, res) => {
   }
 });
 
+//get single Product
+router.get("/:id", async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await Products.findById(productId).populate(
+      "author",
+      "email username"
+    );
+    if (!product) {
+      return res.status(404).send({ message: "Product not found" });
+    }
+    const reviews = await Reviews.find({ productId }).populate(
+      "userId",
+      "username email"
+    );
+    res.status(200).send({ product, reviews });
+  } catch (error) {
+    console.log("Error fetching product", error);
+    res.status(500).send({ message: "Failed to fetching product" });
+  }
+});
+
 module.exports = router;
