a40f8c962d29926501c7896bd02f5accadb205a2
Le Van Cong <conglevan21072001@innofly-software.com>
2024-12-11 10:33:10 +0700

    add get all product

--- src/products/products.route.js
+++ src/products/products.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Products = require("./products.modal");
+const Reviews = require("../reviews/reviews.modal");
 
 const router = express.Router();
 
@@ -10,8 +11,61 @@ router.post("/create-product", async (req, res) => {
       ...req.body,
     });
     const savedProduct = await newProduct.save();
+    const reviews = await Reviews.find({ productId: savedProduct._id });
+    if (reviews.length > 0) {
+      const totalRating = reviews.reduce(
+        (acc, review) => acc + review.rating,
+        0
+      );
+      const averageRating = totalRating / reviews.length;
+      savedProduct.rating = averageRating;
+      await savedProduct.save();
+    }
     res.status(201).send(savedProduct);
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error creating new product", error);
+    res.status(500).send({ message: "Failed to create new product" });
+  }
+});
+
+//get all product
+router.get("/", async (req, res) => {
+  try {
+    const {
+      category,
+      color,
+      minPrice,
+      maxPrice,
+      page = 1,
+      limit = 10,
+    } = req.query;
+    let filter = {};
+    if (category && category !== "all") {
+      filter.category = category;
+    }
+    if (color && color !== "all") {
+      filter.color = color;
+    }
+    if (minPrice && maxPrice) {
+      const min = parseFloat(minPrice);
+      const max = parseFloat(maxPrice);
+      if (!isNaN(min) && !isNaN(max)) {
+        filter.price = { $gte: min, $lte: max };
+      }
+    }
+    const skip = (parseInt(page) - 1) * parseInt(limit);
+    const totalProducts = await Products.countDocuments(filter);
+    const totalPages = Math.ceil(totalProducts / parseInt(limit));
+    const products = await Products.find(filter)
+      .skip(skip)
+      .limit(parseInt(limit))
+      .populate("author", "email")
+      .sort({ createdAt: -1 });
+    res.status(200).send({ products, totalPages, totalProducts });
+  } catch (error) {
+    console.log("Error fetching product", error);
+    res.status(500).send({ message: "Error fetching product" });
+  }
 });
 
 module.exports = router;
--- src/reviews/reviews.modal.js
+++ src/reviews/reviews.modal.js
@@ -1,5 +1,5 @@
-const { Schema, model } = require("mongoose");
-const ReviewSchema = new Schema(
+const mongoose = require("mongoose");
+const ReviewSchema = new mongoose.Schema(
   {
     comment: { type: String, require: true },
     rating: { type: Number, require: true },
